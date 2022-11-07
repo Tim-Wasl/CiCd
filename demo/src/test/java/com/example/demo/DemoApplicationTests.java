c13e1be4a0a872a9fcddfa39fe2e76021a8856c1
Wasl Tim <Tim.Wasl@adesso.de>
2022-11-07 14:50:04 +0100

    bat

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
+	@Test
 	void contextLoads() {
+	  assertEquals(4, "Test".length());
 	}
 
 }
