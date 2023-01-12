69d1f1cec03f34ae4aaa3e4fbdaf476e27385948
Wasl Tim <Tim.Wasl@adesso.de>
2023-01-12 14:51:57 +0100

    reverted test

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     private int a;
     private transient String b;
+    public static final String IP_ADRESS = "http://127.0.0.1:4200";
     
 
     @GetMapping(path = "/test")
